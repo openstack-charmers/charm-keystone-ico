64335eae34dfd3e44de7a71fb9eaac8c6d1f2eab
Aymen Frikha <aymen.frikha@canonical.com>
2019-01-13 16:40:36 -0500

    - Use endpoint unstead of relationbase class - Handle principle keystone release version

--- src/reactive/keystone_ico_handlers.py
+++ src/reactive/keystone_ico_handlers.py
@@ -11,22 +11,35 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import charms.reactive as reactive
+from charms.reactive import endpoint_from_flag
+from charms.reactive import when, when_not
+from charms.reactive import clear_flag, set_flag
 
 from charms_openstack.charm import provide_charm_instance
+from charmhelpers.contrib.openstack.utils import CompareOpenStackReleases
 import charm.openstack.keystone_ico as ico  # noqa
+from charmhelpers.core.hookenv import status_set, config
 
 
-@reactive.when_not('ico.installed')
+@when_not('ico.installed')
+@when('endpoint.keystone-middleware.new-release')
 def install_packages():
-    with provide_charm_instance() as charm_class:
-        charm_class.install()
-    reactive.set_state('ico.installed')
+    principal_keystone = \
+        endpoint_from_flag('endpoint.keystone-middleware.new-release')
+    release = principal_keystone.release_version()
+    if release and CompareOpenStackReleases(release) >= "rocky":
+        status_set('blocked',
+                   '{} release of keystone software '
+                   'is not supported'.format(release))
+    else:
+        with provide_charm_instance() as charm_class:
+            charm_class.install()
+        set_flag('ico.installed')
 
 
-@reactive.when('keystone-middleware.connected')
-def configure_keystone_middleware(principle):
+@when('endpoint.keystone-middleware.connected',
+      'ico.installed')
+def configure_keystone_middleware():
     with provide_charm_instance() as charm_class:
         charm_class.render_keystone_paste_ini(True)
         middleware_configuration = {
@@ -36,20 +49,27 @@ def configure_keystone_middleware(principle):
             },
             "auth": {
                 "methods": "external,password,token,oauth1",
-                "external": "keystone.auth.plugins.external.Domain",
                 "password": "keystone.auth.plugins.password.Password",
                 "token": "keystone.auth.plugins.token.Token",
                 "oauth1": "keystone.auth.plugins.oauth1.OAuth"
             }
         }
-        principle.configure_principal(
+        if config('multi-tenancy'):
+            middleware_configuration['auth'].update(
+                {"external": "keystone.auth.plugins.external.Domain"
+                 })
+        principal_keystone = \
+            endpoint_from_flag('endpoint.keystone-middleware.connected')
+
+        principal_keystone.configure_principal(
             middleware_name=charm_class.service_name,
             configuration=middleware_configuration)
-    reactive.set_state('ico.configured')
 
 
-@reactive.when('keystone-middleware.departing')
+@when_not('endpoint.keystone-middleware.connected')
 def remove_ico_filter():
     with provide_charm_instance() as charm_class:
         charm_class.uninstall()
         charm_class.render_keystone_paste_ini(False)
+    clear_flag('ico.installed')
+    clear_flag('endpoint.keystone-middleware.departed')
--- unit_tests/test_keystone_ico.py
+++ unit_tests/test_keystone_ico.py
@@ -8,23 +8,27 @@ import charms_openstack.test_utils as test_utils
 
 class TestRegisteredHooks(test_utils.TestRegisteredHooks):
 
-    def test_hooks(self):
+    def test_registered_hooks(self):
         # test that the hooks actually registered the relation expressions that
         # are meaningful for this interface: this is to handle regressions.
         # The keys are the function names that the hook attaches to.
         hook_set = {
             'when': {
                 'configure_keystone_middleware':
-                    ('keystone-middleware.connected', ),
-                'remove_ico_filter':
-                    ('keystone-middleware.departing',)
+                    ('endpoint.keystone-middleware.connected',
+                     'ico.installed', ),
+                'install_packages':
+                    ('endpoint.keystone-middleware.new-release', )
+
             },
             'when_not': {
                 'install_packages': ('ico.installed', ),
+                'remove_ico_filter':
+                    ('endpoint.keystone-middleware.connected', ),
             },
         }
         # test that the hooks were registered via the
-        # reactive.keystone_ico_handlers
+        # reactive.keystone_ico_handler
         self.registered_hooks_test_helper(handlers, hook_set, [])
 
 
@@ -39,21 +43,43 @@ class TestHandlers(test_utils.PatchHelper):
         self.provide_charm_instance().__exit__.return_value = None
         return the_charm
 
-    def test_install_packages(self):
+    def _patch_endpoint_from_flag(self):
+        self.patch_object(handlers, 'endpoint_from_flag',
+                          name='endpoint_from_flag',
+                          new=mock.MagicMock())
+
+    def _patch_release_compare(self, release):
+        self.patch_object(handlers, 'CompareOpenStackReleases',
+                          name='CompareOpenStackReleases',
+                          new=mock.MagicMock())
+        self.CompareOpenStackReleases.return_value = release
+
+    def test_install_packages_successfull(self):
+        self._patch_endpoint_from_flag()
+        self._patch_release_compare('pike')
         the_charm = self._patch_provide_charm_instance()
-        self.patch_object(handlers.reactive, 'set_state')
+        self.patch_object(handlers, 'set_flag')
         handlers.install_packages()
         the_charm.install.assert_called_once_with()
-        calls = [mock.call('ico.installed')]
-        self.set_state.assert_has_calls(calls)
+        self.set_flag.assert_called_once_with('ico.installed')
+
+    def test_not_supported_version(self):
+        keystone_release = 'rocky'
+        self._patch_endpoint_from_flag()
+        self.endpoint_from_flag().release_version.return_value = \
+            keystone_release
+        self._patch_release_compare(keystone_release)
+        self.patch_object(handlers, 'set_flag')
+        self.patch_object(handlers, 'status_set')
+        handlers.install_packages()
+        self.set_flag.assert_not_called()
+        self.status_set.assert_called_with(
+            'blocked',
+            'rocky release of keystone software is not supported')
 
     def test_configure_keystone_middleware(self):
         the_charm = self._patch_provide_charm_instance()
-        self.patch_object(handlers.reactive, 'set_state')
-        self.patch_object(handlers.reactive.RelationBase, 'from_state',
-                          return_value=None)
-        principal = mock.MagicMock()
-        handlers.configure_keystone_middleware(principal)
-        the_charm.setup_simple_token_filter.assert_called_once()
+        self._patch_endpoint_from_flag()
+        handlers.configure_keystone_middleware()
         the_charm.get_ico_token.assert_called_once()
-        self.set_state.assert_called_once_with('ico.configured')
+        the_charm.render_keystone_paste_ini.assert_called_once()
